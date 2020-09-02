a1544b21175f3a46bed4034e6e9aecd95785d5db
Poncho <jplyles1212@gmail.com>
2020-09-02 18:23:17 +0900

    Refactor makeGrid function

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
+const grid = document.querySelector('.gridContainer');
+const userInput = document.getSelection('.quantity');
+const resetBtn = document.querySelector('.reset');
+
 // Create a function to create grid
 makeGrid = () => {
-    // Create divs of rows
-    for (var i = 0; i < 16; i++) {
-        var row = document.createElement('div');
-        row.classList.add('row');
-        document.querySelector('#container').appendChild(row);
-        // Create divs of boxes within rows
-        for (var j = 0; j < 16; j++) {
-            var box = document.createElement('div');
-            box.classList.add('box');
-            row.appendChild(box);
-        }
+    // Create grid of divs
+    for (var i = 0; i < 256; i++) {
+        var div = document.createElement('div');
+        div.classList.add('box');
+        grid.appendChild(div);
     }
-}
+};
 
 // Create function that changes box colors
 changeColor = (e) => {
     e.target.classList.replace('box', 'color');
 }
 
-// Create event listener on hover 
-const box = document.querySelector('#container');
-console.dir(box.firstElementChild);  
+// Create event listener on hover  
+const box = document.querySelector('div');
 box.addEventListener('mouseover', changeColor);
-console.log(e)
-    
+
+makeGrid();
